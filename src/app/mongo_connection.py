75b18cd36e5befe0b3647c16f14ca9a4c92ba8e0
Bruna Moura <b.moura@rioanalytics.com.br>
2023-10-14 21:09:32 -0300

    organized dependencies.

--- src/app/mongo_connection.py
+++ src/app/mongo_connection.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
+from decouple import config
 
-client = MongoClient(
-    'mongodb+srv://bruunamoura41:ftb123@cluster0.gz86vmy.mongodb.net/'
-    '?retryWrites=true&w=majority')
+# Obter a connection string a partir das variáveis de ambiente
+MONGO_CONNECTION_STRING = config('MONGO_CONNECTION_STRING')
+
+client = MongoClient(MONGO_CONNECTION_STRING)
 db = client['project_management']
